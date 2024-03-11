32076f94161d267378e3a22889eab2c3881becc2
Aleksander Kolsrud <kols203@gmail.com>
2024-03-11 15:04:17 +0100

    Empty project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, createContext } from 'react'
+import './App.css'
+
+export const DataContext = createContext()
+
+function App() {
+  
+
+  return (
+    <>
+      
+      
+    </>
+  )
+}
+
+export default App
